11999d49f159fb1a8e5fe5e1d8e0cfa5a09486c6
justinsam <justinsam@justins-mbp.home>
2023-01-27 21:44:57 -0500

    Hello World

---
+++ src/main/java/com/mycompany/csc_229_demo/CSC_229_Demo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.csc_229_demo;
+
+/**
+ *
+ * @author justinsam
+ */
+public class CSC_229_Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
